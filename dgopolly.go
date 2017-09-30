1c326d4527fb1c88b69b94957aa589cee9dc6607
Brandon Tomlinson <thebwt@gmail.com>
2017-09-30 12:35:09 -0500

    getSynthSpeech completed

--- dgopolly.go
+++ dgopolly.go
@@ -1,14 +1,19 @@
 package discordgo_polly
 
 import (
+	"encoding/binary"
+	"fmt"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/credentials"
 	session2 "github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/polly"
 	"github.com/bitly/go-simplejson"
 	"github.com/bwmarrin/discordgo"
+	"io"
 	"io/ioutil"
+	"layeh.com/gopus"
 	"log"
+	"sync"
 )
 
 func relax(err error) {
@@ -18,7 +23,7 @@ func relax(err error) {
 }
 
 // To keep this module if you don't want to use the config structure I use, this just returns a static credential
-func loadConfig() aws.Config {
+func LoadConfig() aws.Config {
 	//load our bot config
 	file, err := ioutil.ReadFile("./config.json")
 	if err != nil {
@@ -56,12 +61,12 @@ func loadConfig() aws.Config {
 }
 
 //ultimate goal is to have a function that we can just say
-type dgopolly struct {
+type DgoPolly struct {
 	pollySession polly.Polly
 	pollyConfig  polly.SynthesizeSpeechInput
 }
 
-func newPolly(creds credentials.Credentials, aws aws.Config) (ourPolly dgopolly) {
+func newPolly(creds credentials.Credentials, aws aws.Config) (ourPolly DgoPolly) {
 	session, err := session2.NewSession(&aws)
 	relax(err)
 	ourPolly.pollySession = *polly.New(session)
@@ -78,6 +83,76 @@ func newPolly(creds credentials.Credentials, aws aws.Config) (ourPolly dgopolly)
 	return
 }
 
-func (*dgopolly) say(vc *discordgo.VoiceConnection, text string) {
+func (p *DgoPolly) say(vc *discordgo.VoiceConnection, text string) {
+}
+
+//Returns an Opus channel for play at 48000Hz, what discordGo needs for vc.OpusSend
+func (p *DgoPolly) getSynthSpeech(text string) *chan []byte {
+	p.pollyConfig.Text = &text
+
+	request, response := p.pollySession.SynthesizeSpeechRequest(&p.pollyConfig)
+	err := request.Send()
+	relax(err)
+
+	outputChan := make(chan []byte, 1000)
+	encodeChan := make(chan []int16, 1000)
+
+	opusEncoder, err := gopus.NewEncoder(16000, 1, gopus.Voip)
+	relax(err)
+	opusEncoder.SetBitrate(64 * 1000)
+
+	wg := sync.WaitGroup{}
+
+	loadBuffer := func(input io.Reader) {
+
+		defer func() {
+			fmt.Println("done loading frames")
+			wg.Done()
+			close(encodeChan)
+		}()
+
+		for {
+			//960 is the framesize, 1 is the number of channels the input has
+			inBuf := make([]int16, 960*1)
+			err := binary.Read(input, binary.LittleEndian, &inBuf)
+			if err == io.EOF || err == io.ErrUnexpectedEOF {
+				return
+			}
+			//Upsamping.. apparently
+			// polly does audio in 16KHz, discord needs 48KHz, so we add every frame 3 times.
+			encodeChan <- inBuf
+			encodeChan <- inBuf
+			encodeChan <- inBuf
+
+		}
+	}
+
+	encodeBuffer := func() {
+		defer func() {
+			fmt.Println("Done encoding new frames")
+			wg.Done()
+			close(outputChan)
+		}()
+
+		for {
+			pcm, ok := <-encodeChan
+			if !ok {
+				return
+			}
+			//we want audio in two channels. So the normal 960*2, plus the additional *2 because the source has more
+			// channels
+			opus, err := opusEncoder.Encode(pcm, 960, 960*2*2)
+			relax(err)
+
+			outputChan <- opus
+		}
+	}
+
+	wg.Add(2)
+	go loadBuffer(response.AudioStream)
+	go encodeBuffer()
+	wg.Wait()
+
+	return &outputChan
 
 }
